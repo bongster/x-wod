8a544ade7533c43063f5b11a1351229e39de83d5
bongster88 <bongster88@gmail.com>
2019-05-23 21:36:08 +0900

    setting expo navigation

--- mobile/App.js
+++ mobile/App.js
@@ -1,149 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createBottomTabNavigator } from 'react-navigation';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
+import AppNavigator from './navigation/AppNavigator';
 
-
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    tabBarIcon: ({ focused, tintColor }) => {
-      let iconName = 'ios-information-circle';
-      return <Ionicons name={iconName} size={25} color={tintColor} />;
-    },
-  }
-  
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!1</Text>
-        <Button
-          title="Go to TimerScreen"
-          onPress={ () => this.props.navigation.navigate('TimerScreen') }
-        />
-        <Button
-          title="Go to WODScreen"
-          onPress={ () => this.props.navigation.navigate('WODScreen') }
-        />
-        <Button
-          title="Go to RecordScreen"
-          onPress={ () => this.props.navigation.navigate('RecordScreen') }
-        />
-        <Button
-          title="Go to HistoryScreen"
-          onPress={ () => this.props.navigation.navigate('HistoryScreen') }
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
-  }
-}
-
-class TimerScreen extends React.Component {
-  static navigationOptions = {
-    tabBarIcon: ({ focused, tintColor }) => {
-      let iconName = 'ios-time';
-      return <Ionicons name={iconName} size={25} color={tintColor} />;
-    },
-  }
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>This is TimerScreen</Text>
-      </View>
-    )
-  }
-}
-
-class WODScreen extends React.Component {
-  static navigationOptions = {
-    tabBarIcon: ({ focused, tintColor }) => {
-      let iconName = 'ios-body';
-      return <Ionicons name={iconName} size={25} color={tintColor} />;
-    },
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>This is WODScreen</Text>
-      </View>
-    )
-  }
-}
-
-class RecordScreen extends React.Component {
-  static navigationOptions = {
-    tabBarIcon: ({ focused, tintColor }) => {
-      let iconName = 'ios-clipboard';
-      return <Ionicons name={iconName} size={25} color={tintColor} />;
-    },
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>This is RecordScreen</Text>
-      </View>
-    )
-  }
-}
-
-class HistoryScreen extends React.Component {
-  static navigationOptions = {
-    tabBarIcon: ({ focused, tintColor }) => {
-      let iconName = 'ios-filing';
-      return <Ionicons name={iconName} size={25} color={tintColor} />;
-    },
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>This is HistoryScreen</Text>
-      </View>
-    )
-  }
-}
-
-
-const App = createBottomTabNavigator(
-  {
-    HomeScreen: { screen: HomeScreen },
-    TimerScreen: { screen: TimerScreen },
-    WODScreen: { screen: WODScreen },
-    RecordScreen: { screen: RecordScreen },
-    HistoryScreen: { screen: HistoryScreen },
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-        let iconName = `${navigation.iconName ? navigation.iconName: 'ios-help'}${focused ? '' : '-outline'}`;
-        // You can return any component that you like here! We usually use an
-        // icon component from react-native-vector-icons
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
-      },
-    }),
-    tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      );
     }
-  },
-  {
-    initialRouteName: 'HomeScreen',
   }
-)
 
-export default App;
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
+      ]),
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        ...Icon.Ionicons.font,
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free
+        // to remove this if you are not using it in your app
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
