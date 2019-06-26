f36d4c8b70f93a3e823c713f194f2cd29dc9f6ad
bongster88 <bongster88@gmail.com>
2019-06-26 21:00:34 +0900

    add Login component for checking authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
-import WOD from './components/WOD';
-import Record from './components/Record';
-import Records from './components/Records';
+import Main from './components/Main';
+import Login from './components/Login';
+
 
 function App() {
+  const auth = window.localStorage.getItem('auth')
   return (
     <div className="App">
-      <header>
-        <div className="collapse bg-dark" id="navbarHeader">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-8 col-md-7 py-4">
-                <h4 className="text-white">About</h4>
-                <p className="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-              </div>
-              <div className="col-sm-4 offset-md-1 py-4">
-                <h4 className="text-white">Contact</h4>
-                <ul className="list-unstyled">
-                  <li><a href="#" className="text-white">Follow on Twitter</a></li>
-                  <li><a href="#" className="text-white">Like on Facebook</a></li>
-                  <li><a href="#" className="text-white">Email me</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="navbar navbar-dark bg-dark shadow-sm">
-          <div className="container d-flex justify-content-between">
-            <a href="#" className="navbar-brand d-flex align-items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" aria-hidden="true" className="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-              <strong>Album</strong>
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-        </div>
-      </header>
-      <main role="main">
-        {/* <Home></Home> */}
-        {/* <WOD></WOD> */}
-        {/* <Record></Record> */}
-        <Records></Records>
-
-      </main>
-      <footer className="text-muted">
-        <div className="container">
-          <p className="float-right">
-            <a href="#">Back to top</a>
-          </p>
-          <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
-          <p>New to Bootstrap? <a href="https://getbootstrap.com/">Visit the homepage</a> or read our <a href="/docs/4.3/getting-started/introduction/">getting started guide</a>.</p>
-        </div>
-      </footer>
+      { auth ? (
+        <Main></Main>
+      ) : (
+        <Login></Login>
+      ) }
     </div>
   );
 }
