e938a5516bd753bf93eeff2e9dab8a80949b89a7
Daniel Bong <bongster88@gmail.com>
2020-02-23 08:47:38 +0700

    connect database from client to server
    * add withTracker in client

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,3 +1,11 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
 
-export default new Mongo.Collection('links');
+
+const Links = new Mongo.Collection('links');
+
+const LinkSchema = new SimpleSchema()
+
+
+Links.attachSchema( LinkSchema )
+export default Links;
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,44 +5,54 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { withTracker } from 'meteor/react-meteor-data';
+import Links from '../api/links';
+
 
 import About from './About';
 import Users from './Users';
 import Home from './Home';
 
 
-const App = () => (
-<Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+const App = ({ links }) => (
+  <Router>
+    <div>
+      <nav>
+        {links.map(({ _id, title }) => (
+          <div key={_id}>{title}</div>
+        ))}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+      {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
-export default App;
+export default withTracker(() => {
+  return {
+    links: Links.find({}).fetch(),
+  }
+})(App);
