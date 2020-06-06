5e610ea24122537ed9769078ca4cd0b761d015b9
Daniel <bongster88@gmail.com>
2020-06-06 18:08:53 +0800

    add foods api

---
+++ pages/api/foods.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const foodSchema = new Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String
+  },
+  imageUrl: {
+    type: String,
+    required: false,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+}, {
+  timestamps: true
+})
+
+let foodModel;
+try {
+  foodModel = mongoose.model("foods");
+} catch (e) {
+  foodModel = mongoose.model("foods", foodSchema);
+}
+
+export default async (req, res) => {
+  try {
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+    switch (req.method.toUpperCase()) {
+      case "GET":
+        return res.json(await foodModel.find().exec());
+        break;
+      case "POST":
+        return res.json(await foodModel.create(req.body));
+        break;
+      default:
+        return res.status(405).send(new Error(`Not supported method type: ${req.method}`))
+        break;
+    }
+  } catch (e) {
+    res.status(500).send(e)
+  }
+
+}
