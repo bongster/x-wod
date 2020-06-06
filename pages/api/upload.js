ec4fdb8837992c2c5762cdc96257e86a388972e7
Daniel <bongster88@gmail.com>
2020-06-06 15:48:11 +0800

    upload cloudinary

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,3 +1,39 @@
-export default (req, res) => {
-  res.status(200).json({ uri: 'upload' });
+import { v2 as Cloudinary } from "cloudinary";
+import multer from "multer";
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}.jpg`);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+export default async (req, res) => {
+  if (!process.env.CLOUDINARY_URL) {
+    res.send(new Error("have to set $CLOUDINARY_URL env"));
+    return;
+  }
+  console.log(process.env.CLOUDINARY_URL);
+  const Uploader = upload.single("file");
+  Uploader(req, res, err => {
+    const uniqueFilename = new Date().toISOString();
+    Cloudinary.uploader.upload(req.file.path, { 
+      public_id: `image/${uniqueFilename}`,
+      "crop": "limit", 
+      "tags": "image", 
+      "width": 250, 
+      "height": 250,
+    }, function (err, image) { 
+      if (err)
+        res.send(err)
+      res.status(200).json({ url: image.secure_url });
+    });
+  })
 }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import Container from '@material-ui/core/Container';
+import Card from "../components/Card"
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,29 +15,13 @@ export default function Home({ allPostsData }) {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <Container>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Container>
     </Layout>
   )
 }
