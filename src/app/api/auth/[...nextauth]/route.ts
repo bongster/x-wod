a6a2351c6069ccc6e463800ca6ec96a2d575ab04
Sungju Bong <sungju.bong@thunes.com>
2023-12-22 10:17:02 +0700

    change login provider from credential to google

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,14 @@
 import NextAuth, { AuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-
-function authenticate(email: any, password: any): any {
-  return {
-    user: {
-      email,
-    }
-  };
-}
+import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: AuthOptions = {
   providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-        if (typeof credentials !== "undefined") {
-          const res = await authenticate(credentials.email, credentials.password)
-          if (typeof res !== "undefined") {
-            return { ...res.user, apiToken: res.token }
-          } else {
-            return null
-          }
-        } else {
-          return null
-        }
-      }
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     })
   ],
-  session: { strategy: "jwt" }
+  secret: "process.env.NEXTAUTH_SECRET as string",
 }
 const handler = NextAuth(authOptions);
 
